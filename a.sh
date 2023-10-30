c6a5426dfb0e249337ae6df0b6462505774a2941
Kawai <b20t2028c@edu.tottori-u.ac.jp>
2023-10-30 19:06:30 +0900

    10/30 19:06

---
+++ a.sh
@@ -0,0 +1,4 @@
+python train.py up_down
+python train.py up_lowerleft
+python train.py upperright_down
+python train.py upperright_lowerleft
---
+++ bn.sh
@@ -0,0 +1,4 @@
+python train.py bn_up_down
+python train.py bn_up_lowerleft
+python train.py bn_upperright_down
+python train.py bn_upperright_lowerleft
